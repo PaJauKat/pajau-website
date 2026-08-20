9f9eca1e7ed4e8516a2818bd7acd401d5f839222
PajauKat <mati.sabaku@gmail.com>
2026-08-20 15:38:04 -0400

    cambio de node a vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   redirects: {
       '/rch': 'https://discord.gg/rrhxskVWNg'
   },
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
