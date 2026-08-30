37ec1e48bd841051cbe4b69ccfd1710530a9017b
PajauKat <mati.sabaku@gmail.com>
2026-08-30 09:48:31 -0400

    redirect a releases de kathub

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-      '/rch': 'https://discord.gg/rrhxskVWNg'
+      '/rch': 'https://discord.gg/rrhxskVWNg',
+      '/download-kathub': 'https://github.com/PaJauKat/kathub-go/releases/latest/download/KatHub_Setup.exe'
   },
 
   adapter: vercel()
